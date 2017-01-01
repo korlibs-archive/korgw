0fe58e9a987683da4dcebf9449b593ef47b81ac4
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-01-01 23:11:43 +0100

    Add prettier button styles

--- korui/src/com/soywiz/korui/light/html/HtmlLightComponents.kt
+++ korui/src/com/soywiz/korui/light/html/HtmlLightComponents.kt
@@ -40,7 +40,52 @@ class HtmlLightComponents : LightComponents() {
 			head.appendChild(style);
 		}
 
-		addStyles('input, progress { -webkit-appearance: none; }');
+		var css = [
+			".myButton {",
+			"	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;",
+			"	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;",
+			"	box-shadow:inset 0px 1px 0px 0px #ffffff;",
+			"	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #ffffff), color-stop(1, #f6f6f6));",
+			"	background:-moz-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);",
+			"	background:-webkit-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);",
+			"	background:-o-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);",
+			"	background:-ms-linear-gradient(top, #ffffff 5%, #f6f6f6 100%);",
+			"	background:linear-gradient(to bottom, #ffffff 5%, #f6f6f6 100%);",
+			"	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffffff', endColorstr='#f6f6f6',GradientType=0);",
+			"	background-color:#ffffff;",
+			"	-moz-border-radius:6px;",
+			"	-webkit-border-radius:6px;",
+			"	border-radius:6px;",
+			"	border:1px solid #dcdcdc;",
+			"	display:inline-block;",
+			"	cursor:pointer;",
+			"	color:#666666;",
+			"	font-family:Arial;",
+			"	font-size:15px;",
+			"	font-weight:bold;",
+			"	padding:6px 24px;",
+			"	text-decoration:none;",
+			"	text-shadow:0px 1px 0px #ffffff;",
+			"}",
+			".myButton:hover {",
+			"	background:-webkit-gradient(linear, left top, left bottom, color-stop(0.05, #f6f6f6), color-stop(1, #ffffff));",
+			"	background:-moz-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);",
+			"	background:-webkit-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);",
+			"	background:-o-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);",
+			"	background:-ms-linear-gradient(top, #f6f6f6 5%, #ffffff 100%);",
+			"	background:linear-gradient(to bottom, #f6f6f6 5%, #ffffff 100%);",
+			"	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#f6f6f6', endColorstr='#ffffff',GradientType=0);",
+			"	background-color:#f6f6f6;",
+			"}",
+			".myButton:active {",
+			"	position:relative;",
+			"	top:1px;",
+			"}"
+		].join('\n')
+
+		//addStyles('input, progress { -webkit-appearance: none; }');
+		addStyles(css);
+
 		document.body.style.background = '#f0f0f0';
 		var inputFile = document.createElement('input');
 		inputFile.type = 'file';
@@ -61,7 +106,7 @@ class HtmlLightComponents : LightComponents() {
 				window.mainFrame = e;
 				break;
             case 'container': e = document.createElement('div'); break;
-            case 'button': e = document.createElement('input'); e.type = 'button'; break;
+            case 'button': e = document.createElement('input'); e.className = 'myButton'; e.type = 'button'; break;
 			case 'progress': e = document.createElement('progress'); break;
             case 'image':
                 e = document.createElement('canvas');
