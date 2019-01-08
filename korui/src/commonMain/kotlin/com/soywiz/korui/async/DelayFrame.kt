37fbc5c48f221d283fc58e24feb34f4a7afefbb7
soywiz <soywiz@gmail.com>
2019-01-08 14:16:42 +0100

    Cleanup DelayFrame

--- korui/src/commonMain/kotlin/com/soywiz/korui/async/DelayFrame.kt
+++ korui/src/commonMain/kotlin/com/soywiz/korui/async/DelayFrame.kt
@@ -5,10 +5,6 @@ import com.soywiz.korio.lang.*
 import kotlinx.coroutines.*
 import kotlin.coroutines.*
 
-
-// @TODO: Do this better! (JS should use requestAnimationFrame)
-suspend fun delayNextFrame() = _delayNextFrame()
-
 interface DelayFrame {
     fun delayFrame(continuation: CancellableContinuation<Unit>) {
         //continuation.context.get(ContinuationInterceptor) as? Delay ?: DefaultDelay
@@ -19,26 +15,8 @@ interface DelayFrame {
     }
 }
 
-suspend fun DelayFrame.delayFrame() = suspendCancellableCoroutine<Unit> { c -> delayFrame(c) }
-
 val DefaultDelayFrame: DelayFrame = object : DelayFrame {}
-
 val CoroutineContext.delayFrame: DelayFrame get() = get(ContinuationInterceptor) as? DelayFrame ?: DefaultDelayFrame
 
-
-private suspend fun _delayNextFrame() = coroutineContext.delayFrame.delayFrame()
-suspend fun CoroutineContext.delayNextFrame() = delayFrame.delayFrame()
-
-fun CoroutineScope.animationFrameLoop(callback: suspend (Closeable) -> Unit): Closeable {
-    var running = true
-    val close = Closeable {
-        running = false
-    }
-    launchImmediately {
-        while (running) {
-            callback(close)
-            delayNextFrame()
-        }
-    }
-    return close
-}
+suspend fun DelayFrame.delayFrame() = suspendCancellableCoroutine<Unit> { c -> delayFrame(c) }
+suspend fun delayFrame() = coroutineContext.delayFrame.delayFrame()
