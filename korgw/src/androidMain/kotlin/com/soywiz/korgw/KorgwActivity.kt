4783866e734ac2ac0658a7079528489b983f0879
soywiz <soywiz@gmail.com>
2019-01-26 12:11:48 +0100

    More work on Android

--- korgw/src/androidMain/kotlin/com/soywiz/korgw/KorgwActivity.kt
+++ korgw/src/androidMain/kotlin/com/soywiz/korgw/KorgwActivity.kt
@@ -1,15 +1,17 @@
 package com.soywiz.korgw
 
 import android.app.Activity
-import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import com.soywiz.kgl.KmlGl
 import com.soywiz.kgl.KmlGlAndroid
-import com.soywiz.korag.*
+import com.soywiz.klock.DateTime
+import com.soywiz.korag.AGOpengl
 import com.soywiz.korev.*
 import com.soywiz.korio.Korio
+import com.soywiz.korma.geom.setTo
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -19,6 +21,41 @@ abstract class KorgwActivity : Activity() {
     lateinit var ag: AGOpengl
 
     var fps: Int = 60
+
+    protected val renderEvent = RenderEvent()
+    protected val initEvent = InitEvent()
+    protected val disposeEvent = DisposeEvent()
+    protected val fullScreenEvent = FullScreenEvent()
+    protected val reshapeEvent = ReshapeEvent()
+    protected val keyEvent = KeyEvent()
+    protected val mouseEvent = MouseEvent()
+    protected val touchEvent = TouchEvent()
+    protected val dropFileEvent = DropFileEvent()
+
+    val touches = Array(10) { Touch(it).apply { active = false } }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        gameWindow?.dispatch(touchEvent.apply {
+            this.touch = touches[event.actionIndex]
+            this.type = when (event.action) {
+                MotionEvent.ACTION_DOWN -> TouchEvent.Type.START
+                MotionEvent.ACTION_MOVE -> TouchEvent.Type.MOVE
+                MotionEvent.ACTION_UP -> TouchEvent.Type.END
+                else -> TouchEvent.Type.END
+            }
+            if (type == TouchEvent.Type.START) {
+                this.touch.active = true
+                this.touch.startTime = DateTime.now()
+                this.touch.start.setTo(event.x, event.y)
+            }
+            this.touch.currentTime = DateTime.now()
+            this.touch.current.setTo(event.x, event.y)
+            if (type == TouchEvent.Type.END) {
+                this.touch.active = false
+            }
+        })
+        return true
+    }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,11 +76,31 @@ abstract class KorgwActivity : Activity() {
         }
 
         mGLView = object : GLSurfaceView(this) {
-            private val mRenderer: KorgwRenderer
             init {
                 setEGLContextClientVersion(2)
-                mRenderer = KorgwRenderer()
-                setRenderer(mRenderer)
+                setRenderer(object : GLSurfaceView.Renderer {
+                    override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
+                        //GLES20.glClearColor(0.0f, 0.4f, 0.7f, 1.0f)
+                        gameWindow?.dispatch(initEvent)
+                    }
+
+                    override fun onDrawFrame(unused: GL10) {
+                        //GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+                        gameWindow?.coroutineDispatcher?.executePending()
+                        gameWindow?.dispatch(renderEvent)
+                    }
+
+                    override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
+                        //GLES20.glViewport(0, 0, width, height)
+                        gameWindow?.dispatch(reshapeEvent.apply {
+                            this.x = 0
+                            this.y = 0
+                            this.width = width
+                            this.height = height
+                        })
+                        ag.setViewport(0, 0, width, height)
+                    }
+                })
             }
         }
 
@@ -51,34 +108,6 @@ abstract class KorgwActivity : Activity() {
 
         Korio(this) {
             main()
-        }
-    }
-
-    protected val renderEvent = RenderEvent()
-    protected val initEvent = InitEvent()
-    protected val disposeEvent = DisposeEvent()
-    protected val fullScreenEvent = FullScreenEvent()
-    protected val reshapeEvent = ReshapeEvent()
-    protected val keyEvent = KeyEvent()
-    protected val mouseEvent = MouseEvent()
-    protected val touchEvent = TouchEvent()
-    protected val dropFileEvent = DropFileEvent()
-
-    inner class KorgwRenderer : GLSurfaceView.Renderer {
-        override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
-            //GLES20.glClearColor(0.0f, 0.4f, 0.7f, 1.0f)
-            gameWindow?.dispatch(initEvent)
-        }
-
-        override fun onDrawFrame(unused: GL10) {
-            GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-            gameWindow?.coroutineDispatcher?.executePending()
-            gameWindow?.dispatch(renderEvent)
-        }
-
-        override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
-            GLES20.glViewport(0, 0, width, height)
-            gameWindow?.dispatch(reshapeEvent)
         }
     }
 
